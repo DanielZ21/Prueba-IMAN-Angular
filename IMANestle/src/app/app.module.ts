e46b5de2f9c4e201c52615a22ae69c3fd0045acc
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-09-11 16:23:33 -0300

    first commit

---
+++ IMANestle/src/app/app.module.ts
@@ -0,0 +1,27 @@
+//MÓDULOS
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/modules/material/material.module';
+
+//COMPONENTES
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
