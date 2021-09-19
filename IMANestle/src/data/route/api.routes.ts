9a0476d0752737116dc32b04ca895da12f804e64
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-09-19 01:33:15 -0300

    login 1.2

--- IMANestle/src/data/route/api.routes.ts
+++ IMANestle/src/data/route/api.routes.ts
@@ -3,7 +3,7 @@ import { environment as ENV } from "src/environments/environment"
 
 export const API_ROUTES= {
     AUTH: {
-        LOGIN : `${ENV.apiUrl}Negocio/Identity/Login`,
+        LOGIN : `${ENV.apiUrl}https://jsonplaceholder.typicode.com/users`,
         REGISTER : `Account/registro`,
         TEST_CONNECTION : `Account/TestConnection`,
        
