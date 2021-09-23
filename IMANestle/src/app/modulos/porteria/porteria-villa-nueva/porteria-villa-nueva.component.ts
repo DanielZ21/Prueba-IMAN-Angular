692fb730fb4e714d67c68e9b7de649fbf781f83d
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-09-23 15:03:24 -0300

    porteríaVillaNuevaFront1.0

--- IMANestle/src/app/modulos/porteria/porteria-villa-nueva/porteria-villa-nueva.component.ts
+++ IMANestle/src/app/modulos/porteria/porteria-villa-nueva/porteria-villa-nueva.component.ts
@@ -3,23 +3,16 @@ import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  articulo: string;
+  descripcion: string;
+  cantidad: number;
+  unidad: number;
+  pallets: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {articulo: '', descripcion: '', cantidad: 0, unidad: 0, pallets:''}
+  
 ];
 
 @Component({
@@ -28,7 +21,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./porteria-villa-nueva.component.css']
 })
 export class PorteriaVillaNuevaComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['articulo', 'descripcion', 'cantidad', 'unidad', 'pallets'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   @ViewChild(MatSort) sort!: MatSort;
