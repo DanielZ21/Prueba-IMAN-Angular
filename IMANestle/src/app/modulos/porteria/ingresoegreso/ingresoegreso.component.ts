58c2c51aa463b99436ff559dd5428fefc402633c
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-09-24 09:35:58 -0300

    otro commit

--- IMANestle/src/app/modulos/porteria/ingresoegreso/ingresoegreso.component.ts
+++ IMANestle/src/app/modulos/porteria/ingresoegreso/ingresoegreso.component.ts
@@ -20,6 +20,13 @@ export interface PeriodicElement1 {
   symbol: string;
 }
 
+export interface PeriodicElement2 {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
 const ELEMENT_DATA: PeriodicElement[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
@@ -46,6 +53,19 @@ const ELEMENT_DATA1: PeriodicElement1[] = [
   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
 
+const ELEMENT_DATA2: PeriodicElement2[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 @Component({
   selector: 'app-ingresoegreso',
   templateUrl: './ingresoegreso.component.html',
@@ -55,7 +75,9 @@ export class IngresoegresoComponent implements AfterViewInit, OnInit {
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   displayedColumns1: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns2: string[] = ['position', 'name', 'weight', 'symbol'];
 
+  dataSource2 = ELEMENT_DATA2;
   dataSource = new MatTableDataSource(ELEMENT_DATA);
   dataSource1 = new MatTableDataSource(ELEMENT_DATA1);
 
