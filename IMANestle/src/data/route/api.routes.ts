15a55795bb131fbaa7f68c55cb58b2283bdddb33
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-10-13 09:43:28 -0300

    .

--- IMANestle/src/data/route/api.routes.ts
+++ IMANestle/src/data/route/api.routes.ts
@@ -13,6 +13,7 @@ export const API_ROUTES= {
     INGRESO_FABRICA_TB:{
         GET_INGRESOS: `${ENV.apiUrl}Negocio/IngresoFabricaTB/GetIngresos`,
         GET_NUEVO_INGRESO_FABRICA: `${ENV.apiUrl}Negocio/IngresoFabricaTB/NuevoIngresoFabrica`,
+        GET_INGRESO_FABRICA: `${ENV.apiUrl}Negocio/IngresoFabricaTB/GetIngresoFabrica`,
         POST_INGRESO_FABRICA: `${ENV.apiUrl}Negocio/IngresoFabricaTB/PostIngresoFabrica`,
         GET_PESO_ACTUAL: `${ENV.apiUrl}Negocio/IngresoFabricaTB/GetPesoActual`,
     },             
@@ -49,6 +50,44 @@ export const API_ROUTES= {
     CHOFER: {
         GET_ALL_CHOFERES: `${ENV.apiUrl}Negocio​/Chofer​/GetAllChoferes`,
         GET_ONE_CHOFER: `${ENV.apiUrl}Negocio​/Chofer​/GetOneChofer`,
-    }
+        POST_CHOFER: `${ENV.apiUrl}Negocio​/Chofer​/PostChofer`,
+        DELETE_CHOFER: `${ENV.apiUrl}Negocio​/Chofer​/DeleteChofer`
+    },
+
+     /* ---------- APIS CLIENTES ------------*/
+
+    CLIENTE: {
+        GET_ALL_CLIENTES: `${ENV.apiUrl}Negocio​/Cliente/GetAllCliente`,
+        GET_ONE_CLIENTE: `${ENV.apiUrl}Negocio​/Cliente/GetOneCliente`,
+        POST_CLIENTE: `${ENV.apiUrl}Negocio​/Cliente/PostCliente`,
+        DELETE_CLIENTE: `${ENV.apiUrl}Negocio​/Cliente/DeleteCliente`
+    },
+
+    /* ---------- APIS DESTINOS ------------*/
+
+    DESTINO: {
+        GET_ALL_DESTINOS: `${ENV.apiUrl}Negocio​/Cliente/GetAllDestinos`,
+        GET_ONE_DESTINO: `${ENV.apiUrl}Negocio​/Cliente/GetOneDestino`,
+        POST_DESTINO: `${ENV.apiUrl}Negocio​/Cliente/PostDestino`,
+        DELETE_DESTINO: `${ENV.apiUrl}Negocio​/Cliente/PostDestino`,
+    },
+
+    /* ---------- APIS EXPORTADORES ------------*/
+
+    EXPORTADOR: {
+        GET_ALL_EXPORTADORES: `${ENV.apiUrl}Negocio​/Cliente/GetAllExportadores`,
+        GET_ONE_EXPORTADOR: `${ENV.apiUrl}Negocio​/Cliente/GetOneExportador`,
+        POST_EXPORTADOR: `${ENV.apiUrl}Negocio​/Cliente/PostExportador`,
+        DELETE_EXPÓRTADOR: `${ENV.apiUrl}Negocio​/Cliente/PostExportador`,
+    },
+
+    /* ---------- APIS NACIONALIDADES ------------*/
+
+    NACIONALIDAD: {
+        GET_ALL_NACIONALIDADES: `${ENV.apiUrl}Negocio​/Cliente/GetAllNacionalidades`,
+        GET_ONE_NACIONALIDAD: `${ENV.apiUrl}Negocio​/Cliente/GetOneNacionalidad`,
+        POST_NACIONALIDAD: `${ENV.apiUrl}Negocio​/Cliente/PostNacionalidad`,
+        DELETE_NACIONALIDAD: `${ENV.apiUrl}Negocio​/Cliente/PostNacionalidad`,
+    },
 
 }
